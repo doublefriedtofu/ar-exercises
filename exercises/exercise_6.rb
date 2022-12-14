74350e6612878d89400f0005a4ca672dcfa07037
doublefriedtofu <hurdonghyuk@gmail.com>
2022-12-14 11:24:39 -0500

    Complete exercise6 created employees for stores following the store_id

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,10 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+@store2.employees.create(first_name: "David", last_name: "Smith", hourly_rate: 49)
+@store2.employees.create(first_name: "Julia", last_name: "Patal", hourly_rate: 120)
+@store1.employees.create(first_name: "Bob", last_name: "Polenski", hourly_rate: 160)
+@store2.employees.create(first_name: "Grace", last_name: "Park", hourly_rate: 2)
+@store1.employees.create(first_name: "Sofia", last_name: "Lee", hourly_rate: 182)
+@store1.employees.create(first_name: "Lucy", last_name: "Black", hourly_rate: 888)
